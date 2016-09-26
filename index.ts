61f49d323a3c91bea84a03347168b439c7cc8648
gbuomprisco <giancarlopsk@gmail.com>
2016-09-26 14:23:19 +0100

    Typings resolution fixes

---
+++ index.ts
@@ -0,0 +1,17 @@
+export {
+    RestifyProvider,
+
+    Get,
+    Post,
+    Put,
+    Delete,
+
+    TransformResponse,
+    WithCredentials,
+    Retry,
+    BaseUrl,
+    ResponseType,
+
+    LocalHeaders,
+    GlobalHeaders
+} from './src/index';
